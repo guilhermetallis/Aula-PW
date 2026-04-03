fe4457ad63957e10cacd223a2661fd865e2a7c21
guilhermetallis <guilhermetallismartinho07@gmail.com>
2026-04-03 12:26:40 -0300

    exercícios de PHP 1 ao 5

---
+++ ex2.php
@@ -0,0 +1,8 @@
+<?php
+
+$c = floatval(readline("Digite a temperatura em Celsius: "));
+
+$f = ($c * 9 / 5) + 32;
+
+echo "Temperatura em Fahrenheit: $f\n";
+
---
+++ ex4.php
@@ -0,0 +1,12 @@
+<?php
+
+$distancia = floatval(readline("Digite a distância percorrida (km): "));
+$combustivel = floatval(readline("Digite o total de combustível gasto (litros): "));
+
+if ($combustivel > 0) {
+    $consumo = $distancia / $combustivel;
+    echo "Consumo médio: $consumo km/l\n";
+} else {
+    echo "Valor de combustível inválido\n";
+}
+
