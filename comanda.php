807781f5f066e90385cf588ef9ef00aa627634b2
guilhermetallis <guilhermetallismartinho07@gmail.com>
2026-04-02 23:05:47 -0300

    pizzaria

--- comanda.php
+++ comanda.php
@@ -3,7 +3,7 @@
     $cod = $_POST['cxcodigo'];
     $estoque = array('coca cola', 'pizza margherita', 'pizza pepperoni', 'pizza vegetariana');
     $cliente = array(
-        ['João', 'Maria', 'Pedro', 'Ana'],
+        ['João', 'Maria', 'Pedro', 'Ana','Carlos'],
         ['guilherme', 'lucas', 'marcos', 'fernanda']
         );
         echo "cliente: " , $cliente[0][0] , "<br/>";
