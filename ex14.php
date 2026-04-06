9741e8b662670cc90bb9a11b6b7b3344ee02d80c
guilhermetallis <guilhermetallismartinho07@gmail.com>
2026-04-05 23:48:30 -0300

    exercicios 11 ao 15

---
+++ ex14.php
@@ -0,0 +1,13 @@
+<?php
+
+$n1 = floatval(readline("Digite o primeiro número: "));
+$n2 = floatval(readline("Digite o segundo número: "));
+
+if ($n1 > $n2) {
+    echo "O maior é: $n1\n";
+} elseif ($n2 > $n1) {
+    echo "O maior é: $n2\n";
+} else {
+    echo "Os dois são iguais\n";
+}
+
