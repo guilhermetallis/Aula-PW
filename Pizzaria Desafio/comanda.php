b89759f0b3a63d5d6f27167e92456b35e607afff
guilhermetallis <guilhermetallismartinho07@gmail.com>
2026-04-07 13:10:22 -0300

    arrumando erros

--- Pizzaria Desafio/comanda.php
+++ Pizzaria Desafio/comanda.php
@@ -1,11 +1,11 @@
 <?php
 // pega os dados do formulário
-$codigo - $_POST['cxcodigo'];
-$produto - $_POST['cxproduto'];
-$quantidade - $_POST['cxqtd'];
+$codigo = $_POST['cxcodigo'];
+$codigoProduto = $_POST['cxproduto'];
+$quantidade = $_POST['cxqtd'];
 
-//lista de clientes 
-$cliene = array(
+// lista de clientes
+$clientes = array(
     1 => "Guilherme",
     2 => "Joyjoy",
     3 => "Milena",
@@ -13,34 +13,35 @@ $cliene = array(
     5 => "Fernanda",
     6 => "Pedro",
     7 => "Luiza",
-    8 => "Rafael",  
+    8 => "Rafael"
 );
 
-//lista de produtos
-$produto = array(
-    1 => array('nome' => 'Pizza de Pizza de Queijo', 'preco' => 35.00), 
+// lista de produtos
+$produtos = array(
+    1 => array('nome' => 'Pizza de Queijo', 'preco' => 35.00),
     2 => array('nome' => 'Pizza de Calabresa', 'preco' => 40.00),
-    3 => array('nome' => 'Pizza de Frango','preco' => 45.00),
-    4 => array('nome' => 'Coca-Cola', 'preco' => 8.00),
+    3 => array('nome' => 'Pizza de Frango', 'preco' => 45.00),
+    4 => array('nome' => 'Coca-Cola', 'preco' => 8.00)
 );
 
 // Verifica se o cliente existe
-if (isset($cliente[$codigo])) {
+if (isset($clientes[$codigo])) {
     $nomeCliente = $clientes[$codigo];
 } else {
     $nomeCliente = 'Cliente não encontrado';
 }
+
 // Verifica se o produto existe
-if (isset($produtos[$produto])) {
-    $nomeProduto = $produtos[$produto]['nome'];
-    $valorUnitario = $produtos[$produto]['preco'];
+if (isset($produtos[$codigoProduto])) {
+    $nomeProduto = $produtos[$codigoProduto]['nome'];
+    $valorUnitario = $produtos[$codigoProduto]['preco'];
 } else {
     $nomeProduto = 'Produto não encontrado';
     $valorUnitario = 0;
 }
 
-//Valor total do pedido 
-$toal = $valorUnitario * $quantidade;
+// Valor total do pedido
+$total = $valorUnitario * $quantidade;
 ?>
 
 <!DOCTYPE html>
@@ -51,15 +52,15 @@ $toal = $valorUnitario * $quantidade;
     <title>Comanda do Pedido</title>
 </head>
 <body>
-    <h1>Comanda do pedido</h1>
+    <h1>Comanda do Pedido</h1>
+
     <p><strong>Cliente:</strong> <?php echo $nomeCliente; ?></p>
     <p><strong>Produto:</strong> <?php echo $nomeProduto; ?></p>
     <p><strong>Quantidade:</strong> <?php echo $quantidade; ?></p>
     <p><strong>Valor Unitário:</strong> R$ <?php echo number_format($valorUnitario, 2, ',', '.'); ?></p>
-    <p><strong>Valor Total:</strong> R$ <?php echo number_format($total, 2, ',', '.'); ?></p>    
+    <p><strong>Valor Total:</strong> R$ <?php echo number_format($total, 2, ',', '.'); ?></p>
 
     <br>
-    <a href="index.html">Fazer novo pedido</a>
-    
+    <a href="index.php">Fazer novo pedido</a>
 </body>
 </html>
