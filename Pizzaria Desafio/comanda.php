e7df86fbb955059bb89e1d066e76f1134c5d507b
guilhermetallis <guilhermetallismartinho07@gmail.com>
2026-04-07 00:17:35 -0300

    Pizzarria project

---
+++ Pizzaria Desafio/comanda.php
@@ -0,0 +1,24 @@
+<?php
+// pega os dados do formulário
+$codigo - $_POST['cxcodigo'];
+$produto - $_POST['cxproduto'];
+$quantidade - $_POST['cxqtd'];
+
+//lista de clientes 
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comanda do Pedido</title>
+</head>
+<body>
+    
+</body>
+</html>
